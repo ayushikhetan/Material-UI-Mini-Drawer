02f1f844a758c517398d7be485061f066ab9d0a5
“Ayushi <ayushi.khetan@lifcare.in>
2018-01-23 23:34:41 +0530

    Style changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,52 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import theme from './theme';
 
+const styles={
+  openModalStyle: {
+    padding: '1em',
+    transtion: '0.3s all ease-out',
+    WebkitTransition: '0.3s all ease-out',
+    transform: 'translate3d(258px, 0px, 0px)',
+    marginTop: '64px',
+    textAlign: 'left'
+  },
+  closeModalStyle: {
+    padding: '1em',
+    transtion: '0.3s all ease-out',
+    WebkitTransition: '0.3s all ease-out',
+    transform: 'translate3d(50px, 0px, 0px)',
+    marginTop: '64px',
+    textAlign: 'left'
+  },
+};
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+    this.toggleNavigation = this.toggleNavigation.bind(this);
+  }
+
+  toggleNavigation() {
+    this.setState({open: !this.state.open});
+  }
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
         <div className="App">
-          <Header />
+          <Header
+            title={'Drawer Menu'}
+            openState={this.state.open}
+            toggleNavigation={this.toggleNavigation}
+          />
+          <p
+            style={
+              this.state.open ? styles.openModalStyle : styles.closeModalStyle
+            }
+          >
+            Drawer Menu
+          </p>
         </div>
       </MuiThemeProvider>
     );
